eb519d6362eed2a73fb86cf1ecdd42d9d6b2cc6e
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-06-10 15:48:57 -0400

    Added Map Generator and Death Plane

--- Lab5/Assets/[Scripts]/RobotBehaviour.cs
+++ Lab5/Assets/[Scripts]/RobotBehaviour.cs
@@ -5,19 +5,46 @@ using UnityEngine.AI;
 
 public class RobotBehaviour : MonoBehaviour
 {
-    public NavMeshAgent agent;
+   
     public Transform player;
+    public bool isGrounded;
+
+    private NavMeshAgent agent;
 
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         player = GameObject.FindObjectOfType<PlayerBehaviour>().transform;
+        agent.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(player.position);
+        if (isGrounded)
+        {
+            agent.enabled = true;
+            agent.SetDestination(player.position);
+        }
     }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            GetComponent<CapsuleCollider>().enabled = false;
+            GetComponent<Rigidbody>().useGravity = false;
+           
+            isGrounded = true;
+        }
+    }
+
+    //private void OnCollisionExit(Collision other)
+    //{
+    //    if (other.gameObject.CompareTag("Ground"))
+    //    {
+    //        isGrounded = false;
+    //    }
+    //}
 }
